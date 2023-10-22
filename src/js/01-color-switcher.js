c48a92e8495fb0eb735f9cb12acf9dd06d46bc40
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-22 21:10:14 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let setId = null;
+const refs = {
+  btnClickStart: document.querySelector('[data-start]'),
+  btnClickStop: document.querySelector('[data-stop]'),
+};
 
+refs.btnClickStart.addEventListener('click', onClickStart);
+refs.btnClickStop.addEventListener('click', onClickStop);
+
+function onClickStart(event) {
+  setId = setInterval(chengeBodyColor, 1000);
+  refs.btnClickStart.disabled = true;
+}
+
+function onClickStop(event) {
+  refs.btnClickStart.disabled = false;
+  clearInterval(setId);
+}
+
+function chengeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
