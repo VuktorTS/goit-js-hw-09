81d6c4e677bc204bd1e703a332759882c631751d
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-22 18:10:49 +0000

    Deploying to gh-pages from @ VuktorTS/goit-js-hw-09@c48a92e8495fb0eb735f9cb12acf9dd06d46bc40 🚀

---
+++ 01-color-switcher.5b1d7d3e.js
@@ -0,0 +1,2 @@
+!function(){var t=null,n={btnClickStart:document.querySelector("[data-start]"),btnClickStop:document.querySelector("[data-stop]")};function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}n.btnClickStart.addEventListener("click",(function(c){t=setInterval(a,1e3),n.btnClickStart.disabled=!0})),n.btnClickStop.addEventListener("click",(function(a){n.btnClickStart.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.5b1d7d3e.js.map
