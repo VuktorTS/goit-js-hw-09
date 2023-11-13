1d71f0f38f8eeb049f3ce5ebb5f81babea6e7af5
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-11-13 08:48:02 +0000

    Deploying to gh-pages from @ VuktorTS/goit-js-hw-09@9ac61f009df6a4d7f6fdd75093492944ac380fbe 🚀

--- 01-color-switcher.cdd4f1f1.js
+++
@@ -1,2 +0,0 @@
-let t=null;const n={btnClickStart:document.querySelector("[data-start]"),btnClickStop:document.querySelector("[data-stop]")};function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}n.btnClickStart.addEventListener("click",(function(a){t=setInterval(e,1e3),n.btnClickStart.disabled=!0})),n.btnClickStop.addEventListener("click",(function(e){n.btnClickStart.disabled=!1,clearInterval(t)}));
-//# sourceMappingURL=01-color-switcher.cdd4f1f1.js.map
